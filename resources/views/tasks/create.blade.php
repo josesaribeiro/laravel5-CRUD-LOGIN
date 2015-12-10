39b5c7b8c1f3c68f0ccba2efb59ac2acfb960afe
matiasvillanueva <cmatiasvillanueva@gmail.com>
2015-12-10 18:24:23 +0000

    Laravel CRUD example

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.master')
+
+@section('content')
+
+<h1>Add a New Task</h1>
+<p class="lead">Add to your task list below.</p>
+<hr>
+
+{!! Form::open([
+    'route' => 'tasks.store'
+]) !!}
+
+@include('partials.alerts.errors')
+
+<div class="form-group">
+    {!! Form::label('title', 'Title:', ['class' => 'control-label']) !!}
+    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('description', 'Description:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
+</div>
+
+{!! Form::submit('Create New Task', ['class' => 'btn btn-primary']) !!}
+
+{!! Form::close() !!}
+
+@stop
